7bbedf9c7a74a2edbf4bb225503d35c55fa5f3d7
J888 <0xjohnh@gmail.com>
2021-11-07 19:14:56 -0500

    Revert "Update next.config.js"
    This reverts commit f2a5fb8aa501f5a2598cdad3d6bca9bb89b731ad.

--- next.config.js
+++ next.config.js
@@ -23,8 +23,7 @@ module.exports = (phase) => {
   const images = {
     domains: [
       `${process.env.IMG_S3_BUCKET}.s3.${process.env.S3_REGION}.amazonaws.com`,
-      'images.squarespace-cdn.com',
-      'nftgazer.com' // needed or else NEXT optimized images won't work
+      'images.squarespace-cdn.com'
     ],
   }
 
