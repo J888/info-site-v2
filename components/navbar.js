e40c67d4a242781db1d04280617daa7e2a11c7fd
J888 <0xjohnh@gmail.com>
2021-12-25 21:11:31 -0500

    Navbar facelift

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import styles from "../sass/components/NavBar.module.scss";
 import { SocialIcon } from 'react-social-icons';
 
-const Nav = ({ siteName, twitterUsername }) => (
+const NavOld = ({ siteName, twitterUsername }) => (
   <nav className={styles.navBar}>
     <Columns className={styles.navBarItems}>
       <Columns.Column size={3} className={styles.navBarColumn1}>
@@ -34,7 +34,52 @@ const Nav = ({ siteName, twitterUsername }) => (
         </Heading>
       </Columns.Column>
 
-      <Columns.Column size={3} className={styles.navBarColumn3}>
+      <Columns.Column size={2} className={styles.navBarColumn3}>
+        <div className={styles.section3}>
+          <SocialIcon
+            url={`https://twitter.com/${twitterUsername}`}
+            bgColor={"rgb(29, 161, 242)"}
+            color={"rgb(29, 161, 242)"}
+            style={{ height: 33, width: 33 }}
+          />
+        </div>
+      </Columns.Column>
+    </Columns>
+  </nav>
+);
+
+const Nav = ({ siteName, twitterUsername }) => (
+  <nav className={styles.navBar}>
+    <Columns className={styles.navBarItems}>
+      <Columns.Column size={2} className={styles.navBarColumn1}>
+        <Heading>
+          <Link href="/" passHref>
+            <a>{siteName}</a>
+          </Link>
+        </Heading>
+      </Columns.Column>
+
+      <Columns.Column size={9} className={styles.navBarColumn2}>
+        <div className={styles.section1}>
+            <Link href="/" className={styles.navItem}>
+              Front Page
+            </Link>
+            <Link href="/posts/news" className={styles.navItem}>
+              News
+            </Link>
+            <Link href="/posts/rarity" className={styles.navItem}>
+              Rarity
+            </Link>
+            <Link href="/posts" className={styles.navItem}>
+              All
+            </Link>
+            <Link href="/about" className={styles.navItem}>
+              About
+            </Link>
+          </div>
+      </Columns.Column>
+
+      <Columns.Column size={1} className={styles.navBarColumn3}>
         <div className={styles.section3}>
           <SocialIcon
             url={`https://twitter.com/${twitterUsername}`}
