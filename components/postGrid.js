8911b33a292595c6916db3e17cd18f4af49676c2
J888 <0xjohnh@gmail.com>
2022-04-09 08:03:20 -0400

    cleanup unused

--- components/postGrid.js
+++ components/postGrid.js
@@ -1,54 +1,6 @@
-import Link from "next/link";
 import styles from "../sass/components/PostGrid.module.scss";
-import TextColoredBackground from "./textColoredBackground";
 import PostGridItemV2 from "./postGridItemV2";
 import React from "react";
-
-const PostGridItem = ({ link, imageUrl, title, category, createdAt }) => (
-  <article className={styles.gridItem}>
-    <div className={styles.imageWrapper}>
-      <Link href={link} passHref>
-        <a>
-          <img src={imageUrl} alt={title}></img>
-        </a>
-      </Link>
-    </div>
-
-    <div className={styles.gridItemText}>
-      <Link href={link} passHref>
-        <a>
-          <div className={styles.gridItemTextMeta}>
-            <TextColoredBackground>
-              {category}
-            </TextColoredBackground>
-
-            <span style={{marginLeft: '0.5rem'}}>
-              {new Date(createdAt).toLocaleDateString("en-US", {
-                weekday: "short",
-                year: "numeric",
-                month: "short",
-                day: "numeric",
-              })}
-            </span>
-            {/* {category} |{" "}
-            {new Date(createdAt).toLocaleDateString("en-US", {
-              weekday: "long",
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })}{" "} */}
-          </div>
-        </a>
-      </Link>
-
-      <Link href={link} passHref>
-        <a>
-          <h3 className={styles.gridItemTextTitle}>{title}</h3>
-        </a>
-      </Link>
-    </div>
-  </article>
-);
 
 const PostGrid = ({ posts, heading, subHeading }) => (
   <React.Fragment>
