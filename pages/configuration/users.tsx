de71bd3a157a7df3b6c3b39effcfb58c7de0a917
J888 <0xjohnh@gmail.com>
2022-10-03 20:08:44 -0400

    make it look better

--- pages/configuration/users.tsx
+++ pages/configuration/users.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Form, Table } from "react-bulma-components";
+import { Box, Button, Columns, Form, Table } from "react-bulma-components";
 import React, { useEffect, useState } from "react";
 import styles from "../../sass/pages/configuration/Users.module.scss";
 import Divider from "../../components/divider";
@@ -16,7 +16,8 @@ type User = {
 
 const AddNewUser = ({
   handleUsernameUpdate, handlePasswordUpdate,
-  handleRetypedPasswordUpdate, handleSaveClick, handleAdminCheckboxChange,
+  handleRetypedPasswordUpdate, handleSaveClick,
+  handleCancelClick, handleAdminCheckboxChange,
   saveButtonDisabled }) => {
   return (
     <Box className={styles.editForm}>
@@ -34,7 +35,10 @@ const AddNewUser = ({
         make user an admin
       </Form.Checkbox>
       <Divider size={'xxs'} />
-      <Button onClick={handleSaveClick} disabled={saveButtonDisabled}>Save</Button>
+      <Button.Group>
+        <Button onClick={handleSaveClick} disabled={saveButtonDisabled} color='success'>Save</Button>
+        <Button onClick={handleCancelClick} color='danger'>Cancel</Button>
+      </Button.Group>
     </Box>
   )
 }
@@ -108,145 +112,166 @@ const Users = ({}) => {
   return (
     <div>
       <ConfigurationPagesWrapper>
-        <h1>Configure Users</h1>
-        <Table>
-          <thead>
-            <tr>
-              <th>
-                <Button onClick={() => {setCreatingNewUser(!creatingNewUser)}}>+</Button>
-              </th>
-              <th>Username</th>
-              <th>Admin?</th>
-              <th>Type</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
+        <Columns>
+          <Columns.Column>
+            <Box>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>
+                      <Button onClick={() => {setCreatingNewUser(!creatingNewUser)}}
+                              disabled={!!creatingNewUser}>
+                        +
+                      </Button>
+                    </th>
+                    <th>Username</th>
+                    <th>Admin?</th>
+                    <th>Type</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+
+                {
+                  usersMap &&
+                    <tbody>
+                    {Object.keys(usersMap).map((username, i) => (
+                      <tr key={`user-row-${username}`}>
+                        <td>{i + 1}.</td>
+                        <td>{username}</td>
+                        <td>{usersMap[username].admin ? "Yes" : "No"}</td>
+                        <td>{usersMap[username].type || "-"}</td>
+                        <td>
+                          <UserActions
+                            isAdmin={usersMap[username].admin}
+                            handleEditClick={() => {
+                              setEditingUser(username);
+                            }}
+                            handleDeleteClick={async () => {
+                              await deleteUser(username);
+                              await fetchAllUsers();
+                            }}
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                }
+
+              </Table>
+            </Box>
+          </Columns.Column>
+
+          {!!editingUser && (
+              <Columns.Column>
+                <Box className={styles.editForm}>
+                  <h3>
+                    Editing <u>{editingUser}</u>
+                  </h3>
+                  <Divider size={"xs"} />
+                  <label>Change password: </label>
+                  <input
+                    placeholder="New password"
+                    type="password"
+                    onChange={(e) => {
+                      setNewPassword(e.target.value);
+                    }}
+                  />
+                  <input
+                    placeholder="Type new password again"
+                    type="password"
+                    onChange={(e) => {
+                      setNewPasswordRetyped(e.target.value);
+                    }}
+                  />
+                  <Divider size={"xxs"} />
+                  {newPassword !== newPasswordRetyped && (
+                    <p>Passwords do not match</p>
+                  )}
+                  <Divider size={"xxs"} />
+                  <Button.Group>
+                    <Button
+                      onClick={async () => {
+                        if (
+                          confirm("Please confirm that you want to change the password")
+                        ) {
+                          let res = await axios.put("/api/users/update", {
+                            user: {
+                              username: editingUser,
+                              password: newPassword,
+                            },
+                          });
+
+                          if (res.status === 200) {
+                            alert("update was successful");
+                          }
+                        }
+                      }}
+                      style={{ width: "fit-content" }}
+                      size={"sm"}
+                      disabled={newPassword !== newPasswordRetyped}
+                      color='success'
+                    >
+                      Save
+                    </Button>
+
+                    <Button
+                      onClick={() => {
+                        setEditingUser(null);
+                      }}
+                      style={{ width: "fit-content" }}
+                      size={"sm"}
+                      color='danger'
+                    >
+                      Cancel
+                    </Button>
+                  </Button.Group>
+                </Box>
+              </Columns.Column>
+          )}
 
           {
-            usersMap &&
-              <tbody>
-              {Object.keys(usersMap).map((username, i) => (
-                <tr key={`user-row-${username}`}>
-                  <td>{i + 1}.</td>
-                  <td>{username}</td>
-                  <td>{usersMap[username].admin ? "Yes" : "No"}</td>
-                  <td>{usersMap[username].type || "-"}</td>
-                  <td>
-                    <UserActions
-                      isAdmin={usersMap[username].admin}
-                      handleEditClick={() => {
-                        setEditingUser(username);
-                      }}
-                      handleDeleteClick={async () => {
-                        await deleteUser(username);
+            creatingNewUser && 
+              <Columns.Column>
+                <AddNewUser
+                  handleUsernameUpdate={setNewUserUsername}
+                  handlePasswordUpdate={setNewUserNewPassword}
+                  handleRetypedPasswordUpdate={setNewUserNewPasswordRetyped}
+                  handleAdminCheckboxChange={setNewUserIsAdmin}
+                  handleSaveClick={async () => {
+                    if (confirm(`are you sure you want to create new user ${newUserUsername}, admin:${newUserIsAdmin}`)) {
+                      let createUserResponse = await axios.post(`/api/users/create`, {
+                        user: {
+                          username: newUserUsername,
+                          password: newUserNewPassword,
+                          isAdmin: newUserIsAdmin,
+                        },
+                      });
+
+                      if (createUserResponse.status === 201) {
+                        alert(`${newUserUsername} created successfully!`);
+                        setNewUserNewPassword(undefined);
+                        setNewUserNewPasswordRetyped(undefined);
+                        setNewUserUsername(undefined);
+                        setNewUserIsAdmin(false);
+                        setCreatingNewUser(false);
                         await fetchAllUsers();
-                      }}
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
+                      }
+                    }
+                  }}
+                  handleCancelClick={
+                    () => {
+                      setCreatingNewUser(false);
+                    }
+                  }
+                  saveButtonDisabled={
+                    (!newUserNewPassword && !newUserNewPasswordRetyped) ||
+                    (newUserNewPassword !== newUserNewPasswordRetyped)
+                  }
+                  
+                />
+              </Columns.Column>
           }
-
-        </Table>
-
-        {!!editingUser && (
-          <div className={styles.editForm}>
-            <h3>
-              Editing <u>{editingUser}</u>
-            </h3>
-            <Divider size={"xs"} />
-            <label>Change password: </label>
-            <input
-              placeholder="New password"
-              type="password"
-              onChange={(e) => {
-                setNewPassword(e.target.value);
-              }}
-            />
-            <input
-              placeholder="Type new password again"
-              type="password"
-              onChange={(e) => {
-                setNewPasswordRetyped(e.target.value);
-              }}
-            />
-            <Divider size={"xxs"} />
-            {newPassword !== newPasswordRetyped && (
-              <p>Passwords do not match</p>
-            )}
-            <Divider size={"xxs"} />
-            <Button
-              onClick={async () => {
-                if (
-                  confirm("Please confirm that you want to change the password")
-                ) {
-                  let res = await axios.put("/api/users/update", {
-                    user: {
-                      username: editingUser,
-                      password: newPassword,
-                    },
-                  });
-
-                  if (res.status === 200) {
-                    alert("update was successful");
-                  }
-                }
-              }}
-              style={{ width: "fit-content" }}
-              size={"sm"}
-              disabled={newPassword !== newPasswordRetyped}
-            >
-              Save
-            </Button>
-
-            <Button
-              onClick={() => {
-                setEditingUser(null);
-              }}
-              style={{ width: "fit-content" }}
-              size={"sm"}
-            >
-              Cancel
-            </Button>
-          </div>
-        )}
-
-        {
-          creatingNewUser && 
-            <AddNewUser
-              handleUsernameUpdate={setNewUserUsername}
-              handlePasswordUpdate={setNewUserNewPassword}
-              handleRetypedPasswordUpdate={setNewUserNewPasswordRetyped}
-              handleAdminCheckboxChange={setNewUserIsAdmin}
-              handleSaveClick={async () => {
-                if (confirm(`are you sure you want to create new user ${newUserUsername}, admin:${newUserIsAdmin}`)) {
-                  let createUserResponse = await axios.post(`/api/users/create`, {
-                    user: {
-                      username: newUserUsername,
-                      password: newUserNewPassword,
-                      isAdmin: newUserIsAdmin,
-                    },
-                  });
-
-                  if (createUserResponse.status === 201) {
-                    alert(`${newUserUsername} created successfully!`);
-                    setNewUserNewPassword(undefined);
-                    setNewUserNewPasswordRetyped(undefined);
-                    setNewUserUsername(undefined);
-                    setNewUserIsAdmin(false);
-                    setCreatingNewUser(false);
-                    await fetchAllUsers();
-                  }
-                }
-              }}
-              saveButtonDisabled={
-                (!newUserNewPassword && !newUserNewPasswordRetyped) ||
-                (newUserNewPassword !== newUserNewPasswordRetyped)
-              }
-            />
-        }
-
+        </Columns>
       </ConfigurationPagesWrapper>
     </div>
   );
